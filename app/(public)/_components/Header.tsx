ac708499790833012f3227d4e4b2fc6d8169c26c
rezen <rezen.khadgi@gmail.com>
2025-12-26 00:14:45 +0545

    error fix

--- app/(public)/_components/Header.tsx
+++ app/(public)/_components/Header.tsx
@@ -14,7 +14,7 @@ const NavLinks = [
 
 export default function Header() {
   const pathname = usePathname();
-  const [open, setOpen] = useState();
+  const [open, setOpen] = useState(false);
 
   const isActive = (href: string) =>
     href === "/" ? pathname === "/" : pathname?.startsWith(href);
@@ -67,6 +67,31 @@ export default function Header() {
                     Sign up
                   </Link>
                 </div>
+                {/* toogle menu */}
+                <button
+                  type="button"
+                  onClick={() => setOpen((v) => !v)}
+                  aria-label="Toggle menu"
+                  aria-expanded={open}
+                  className="md:hidden inline-flex h-9 w-9 p-2 items-center justify-center rounded-md border  border-black/80 bg-white/50 "
+                >
+                  {open ? (
+                    <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
+                      <title>close</title>
+                      <path
+                        d="M2.64,1.27L7.5,6.13l4.84-4.84C12.5114,1.1076,12.7497,1.0029,13,1c0.5523,0,1,0.4477,1,1&#xA;&#x9;c0.0047,0.2478-0.093,0.4866-0.27,0.66L8.84,7.5l4.89,4.89c0.1648,0.1612,0.2615,0.3796,0.27,0.61c0,0.5523-0.4477,1-1,1&#xA;&#x9;c-0.2577,0.0107-0.508-0.0873-0.69-0.27L7.5,8.87l-4.85,4.85C2.4793,13.8963,2.2453,13.9971,2,14c-0.5523,0-1-0.4477-1-1&#xA;&#x9;c-0.0047-0.2478,0.093-0.4866,0.27-0.66L6.16,7.5L1.27,2.61C1.1052,2.4488,1.0085,2.2304,1,2c0-0.5523,0.4477-1,1-1&#xA;&#x9;C2.2404,1.0029,2.4701,1.0998,2.64,1.27z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                      <title>menu</title>
+                      <path
+                        d="M2 3h12a1 1 0 0 1 0 2H2a1 1 0 1 1 0-2zm0 4h12a1 1 0 0 1 0 2H2a1 1 0 1 1 0-2zm0 4h12a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2z"
+                        id="a"
+                      />
+                    </svg>
+                  )}
+                </button>
               </div>
             </div>
           </div>
